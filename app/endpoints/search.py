0db4ef35396960a21846561cbc91648ae61f4488
Eledoux <lawanledoux@gmail.com>
2024-10-11 11:51:48 +0200

    [feat](search): route the endpoint with vectors, keywords, hybrid request param

--- app/endpoints/search.py
+++ app/endpoints/search.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter, Security
 
 from app.helpers import SearchOnInternet
-from app.schemas.search import Searches, SearchRequest
+from app.schemas.search import Search, Searches, SearchRequest
 from app.schemas.security import User
 from app.utils.lifespan import clients
 from app.utils.security import check_api_key
@@ -13,26 +15,40 @@ router = APIRouter()
 @router.post("/search")
 async def search(request: SearchRequest, user: User = Security(check_api_key)) -> Searches:
     """
-    Similarity search for chunks in the vector store or on the internet.
+    Endpoint to search on the internet or in the vector store or using keywords.
     """
 
     data = []
     if INTERNET_COLLECTION_ID in request.collections:
-        request.collections.remove(INTERNET_COLLECTION_ID)
-        internet = SearchOnInternet(models=clients.models)
-        if len(request.collections) > 0:
-            collection_model = clients.vectors.get_collections(collection_ids=request.collections, user=user)[0].model
-        else:
-            collection_model = None
-        data.extend(internet.search(prompt=request.prompt, n=4, model_id=collection_model, score_threshold=request.score_threshold))
+        data.extend(_get_internet_search_result(request, user))
 
     if len(request.collections) > 0:
-        data.extend(
-            clients.vectors.search(
-                prompt=request.prompt, collection_ids=request.collections, k=request.k, score_threshold=request.score_threshold, user=user
-            )
-        )
+        if request.method in ["vectors", "hybrid"]:
+            data.extend(_get_vectors_search_result(request, user))
+        if request.method in ["keywords", "hybrid"]:
+            data.extend(_get_keywords_search_result(request, user))
 
     data = sorted(data, key=lambda x: x.score, reverse=False)[: request.k]
 
     return Searches(data=data)
+
+
+def _get_internet_search_result(request: SearchRequest, user: User) -> List[Search]:
+    request.collections.remove(INTERNET_COLLECTION_ID)
+    internet = SearchOnInternet(models=clients.models)
+    if len(request.collections) > 0:
+        collection_model = clients.vectors.get_collections(collection_ids=request.collections, user=user)[0].model
+    else:
+        collection_model = None
+    return internet.search(prompt=request.prompt, n=4, model_id=collection_model, score_threshold=request.score_threshold)
+
+
+def _get_vectors_search_result(request: SearchRequest, user: User) -> List[Search]:
+    return clients.vectors.search(
+        prompt=request.prompt, collection_ids=request.collections, k=request.k, score_threshold=request.score_threshold, user=user
+    )
+
+
+def _get_keywords_search_result(request: SearchRequest, user: User) -> List[Search]:
+    # TODO: Implement keywords search
+    return []
