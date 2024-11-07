8b4ff4b75c126420417fdd7d9b7df24c4ba7bdf3
leoguillaume <leo.guillaume@modernisation.gouv.fr>
2024-11-07 17:50:39 +0100

    feat: remove streamlit localstorage

--- ui/config.py
+++ ui/config.py
@@ -3,6 +3,5 @@ import os
 BASE_URL = os.getenv("BASE_URL", "http://localhost:8080/v1")
 EMBEDDINGS_MODEL_TYPE = "text-embeddings-inference"
 LANGUAGE_MODEL_TYPE = "text-generation"
-LOCAL_STORAGE_KEY = "albertApiKey"
 INTERNET_COLLECTION_ID = "internet"
 PRIVATE_COLLECTION_TYPE = "private"
