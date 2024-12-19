1071cb0281c56436f2f2707d89c8fc3e42f58242
Luis Arias <luis@inclusif.eu>
2024-12-19 18:46:03 +0100

    feat: :wrench: Add refugies.info to internet domain whitelist (#113)
    * feat: :wrench: Add refugies.info to internet domain whitelist
    
    * style: :recycle: Add trailing comma

--- app/helpers/_internetmanager.py
+++ app/helpers/_internetmanager.py
@@ -46,6 +46,7 @@ class InternetManager:
         "ouest-france.fr",
         "lemonde.fr",
         "leparisien.fr",
+        "refugies.info",
     ]
 
     USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36"
