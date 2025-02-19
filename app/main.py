21e76700212280795d868177176fcac90d81b03b
Julien Almarcha <ext-julien.almarcha@flowbird.group>
2025-02-19 19:38:17 +0100

    add create_application to customize application for test

--- app/main.py
+++ app/main.py
@@ -9,45 +9,52 @@ from app.utils.lifespan import lifespan
 from app.utils.security import check_admin_api_key, check_api_key
 from app.utils.settings import settings
 
-app = FastAPI(
-    title=settings.app_name,
-    version=settings.app_version,
-    description=settings.app_description,
-    contact={"url": settings.app_contact_url, "email": settings.app_contact_email},
-    licence_info={"name": "MIT License", "identifier": "MIT"},
-    lifespan=lifespan,
-    docs_url="/swagger",
-    redoc_url="/documentation",
-)
 
-# Prometheus metrics
-app.instrumentator = Instrumentator().instrument(app=app)
+def create_application(middleware=True) -> FastAPI:
+    app = FastAPI(
+        title=settings.app_name,
+        version=settings.app_version,
+        description=settings.app_description,
+        contact={"url": settings.app_contact_url, "email": settings.app_contact_email},
+        licence_info={"name": "MIT License", "identifier": "MIT"},
+        lifespan=lifespan,
+        docs_url="/swagger",
+        redoc_url="/documentation",
+    )
 
-# Middlewares
-app.add_middleware(middleware_class=SlowAPIASGIMiddleware)
-app.add_middleware(middleware_class=MetricsMiddleware)
+    if middleware:
+        # Prometheus metrics
+        app.instrumentator = Instrumentator().instrument(app=app)
+
+        # Middlewares
+        app.add_middleware(middleware_class=SlowAPIASGIMiddleware)
+        app.add_middleware(middleware_class=MetricsMiddleware)
+
+    # Monitoring
+    @app.get(path="/health", tags=["Monitoring"])
+    def health(user: User = Security(dependency=check_api_key)) -> Response:
+        """
+        Health check.
+        """
+
+        return Response(status_code=200)
+
+    if middleware:
+        app.instrumentator.expose(app=app, should_gzip=True, tags=["Monitoring"], dependencies=[Depends(dependency=check_admin_api_key)])
+
+    app.include_router(router=models.router, tags=["Models"], prefix="/v1")
+    app.include_router(router=chat.router, tags=["Chat"], prefix="/v1")
+    app.include_router(router=completions.router, tags=["Completions"], prefix="/v1")
+    app.include_router(router=embeddings.router, tags=["Embeddings"], prefix="/v1")
+    app.include_router(router=audio.router, tags=["Audio"], prefix="/v1")
+    app.include_router(router=rerank.router, tags=["Reranking"], prefix="/v1")
+    app.include_router(router=search.router, tags=["Search"], prefix="/v1")
+    app.include_router(router=collections.router, tags=["Collections"], prefix="/v1")
+    app.include_router(router=files.router, tags=["Files"], prefix="/v1")
+    app.include_router(router=documents.router, tags=["Documents"], prefix="/v1")
+    app.include_router(router=chunks.router, tags=["Chunks"], prefix="/v1")
+
+    return app
 
 
-# Monitoring
-@app.get(path="/health", tags=["Monitoring"])
-def health(user: User = Security(dependency=check_api_key)) -> Response:
-    """
-    Health check.
-    """
-
-    return Response(status_code=200)
-
-
-app.instrumentator.expose(app=app, should_gzip=True, tags=["Monitoring"], dependencies=[Depends(dependency=check_admin_api_key)])
-
-app.include_router(router=models.router, tags=["Models"], prefix="/v1")
-app.include_router(router=chat.router, tags=["Chat"], prefix="/v1")
-app.include_router(router=completions.router, tags=["Completions"], prefix="/v1")
-app.include_router(router=embeddings.router, tags=["Embeddings"], prefix="/v1")
-app.include_router(router=audio.router, tags=["Audio"], prefix="/v1")
-app.include_router(router=rerank.router, tags=["Reranking"], prefix="/v1")
-app.include_router(router=search.router, tags=["Search"], prefix="/v1")
-app.include_router(router=collections.router, tags=["Collections"], prefix="/v1")
-app.include_router(router=files.router, tags=["Files"], prefix="/v1")
-app.include_router(router=documents.router, tags=["Documents"], prefix="/v1")
-app.include_router(router=chunks.router, tags=["Chunks"], prefix="/v1")
+app = create_application()
