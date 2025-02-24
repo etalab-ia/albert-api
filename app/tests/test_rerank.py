52705c01e77f653f50162a2b09a2b57db971ba64
Julien Almarcha <ext-julien.almarcha@flowbird.group>
2025-02-24 16:21:10 +0100

    update tests with test_client

--- app/tests/test_rerank.py
+++ app/tests/test_rerank.py
@@ -7,8 +7,9 @@ from app.utils.variables import MODEL_TYPE__EMBEDDINGS, MODEL_TYPE__LANGUAGE, MO
 
 
 @pytest.fixture(scope="module")
-def setup(args, session_user):
-    response = session_user.get(f"{args["base_url"]}/models")
+def setup(args, test_client):
+    test_client.headers = {"Authorization": f"Bearer {args['api_key_user']}"}
+    response = test_client.get(f"{args['base_url']}/models")
     assert response.status_code == 200, f"error: retrieve models ({response.status_code})"
     response_json = response.json()
 
@@ -24,40 +25,44 @@ def setup(args, session_user):
     yield LANGUAGE_MODEL_ID, RERANK_MODEL_ID, EMBEDDINGS_MODEL_ID
 
 
-@pytest.mark.usefixtures("args", "session_user", "setup")
+@pytest.mark.usefixtures("args", "setup", "test_client")
 class TestRerank:
-    def test_rerank_with_language_model(self, args, session_user, setup):
+    def test_rerank_with_language_model(self, args, test_client, setup):
         """Test the POST /rerank with a language model."""
         LANGUAGE_MODEL_ID, _, _ = setup
+        test_client.headers = {"Authorization": f"Bearer {args['api_key_user']}"}
         params = {"model": LANGUAGE_MODEL_ID, "prompt": "Sort these sentences by relevance.", "input": ["Sentence 1", "Sentence 2", "Sentence 3"]}
-        response = session_user.post(f"{args["base_url"]}/rerank", json=params)
+        response = test_client.post(f"{args['base_url']}/rerank", json=params)
         assert response.status_code == 200, f"error: rerank with language model ({response.status_code})"
 
         response_json = response.json()
         reranks = Reranks(**response_json)
         assert isinstance(reranks, Reranks)
 
-    def test_rerank_with_rerank_model(self, args, session_user, setup):
+    def test_rerank_with_rerank_model(self, args, test_client, setup):
         """Test the POST /rerank with a rerank model."""
         _, RERANK_MODEL_ID, _ = setup
+        test_client.headers = {"Authorization": f"Bearer {args['api_key_user']}"}
         params = {"model": RERANK_MODEL_ID, "prompt": "Sort these sentences by relevance.", "input": ["Sentence 1", "Sentence 2", "Sentence 3"]}
-        response = session_user.post(f"{args["base_url"]}/rerank", json=params)
+        response = test_client.post(f"{args['base_url']}/rerank", json=params)
         assert response.status_code == 200, f"error: rerank with rerank model ({response.status_code})"
 
         response_json = response.json()
         reranks = Reranks(**response_json)
         assert isinstance(reranks, Reranks)
 
-    def test_rerank_with_wrong_model_type(self, args, session_user, setup):
+    def test_rerank_with_wrong_model_type(self, args, test_client, setup):
         """Test the POST /rerank with a wrong model type."""
         _, _, EMBEDDINGS_MODEL_ID = setup
+        test_client.headers = {"Authorization": f"Bearer {args['api_key_user']}"}
         params = {"model": EMBEDDINGS_MODEL_ID, "prompt": "Sort these sentences by relevance.", "input": ["Sentence 1", "Sentence 2", "Sentence 3"]}
-        response = session_user.post(f"{args["base_url"]}/rerank", json=params)
+        response = test_client.post(f"{args['base_url']}/rerank", json=params)
         assert response.status_code == 422, f"error: rerank with wrong model type ({response.status_code})"
 
-    def test_rerank_with_unknown_model(self, args, session_user, setup):
+    def test_rerank_with_unknown_model(self, args, test_client, setup):
         """Test the POST /rerank with an unknown model."""
         _, _, _ = setup
+        test_client.headers = {"Authorization": f"Bearer {args['api_key_user']}"}
         params = {"model": "unknown", "prompt": "Sort these sentences by relevance.", "input": ["Sentence 1", "Sentence 2", "Sentence 3"]}
-        response = session_user.post(f"{args["base_url"]}/rerank", json=params)
+        response = test_client.post(f"{args['base_url']}/rerank", json=params)
         assert response.status_code == 404, f"error: rerank with unknown model ({response.status_code})"
