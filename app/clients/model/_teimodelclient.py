89eccd11054104c5cfeadfe4282d591bd87436bb
leoguillaume <leo.guillaume@modernisation.gouv.fr>
2025-02-24 15:56:56 +0100

    fix: base_url to api_url

--- app/clients/model/_teimodelclient.py
+++ app/clients/model/_teimodelclient.py
@@ -8,29 +8,40 @@ import requests
 from app.clients.model._basemodelclient import BaseModelClient
 from app.helpers.rerank import TEIRerank
 from app.schemas.models import Model, Models
-from app.schemas.settings import ModelClient as ModelClientSettings
+from app.utils.variables import (
+    ENDPOINT__AUDIO_TRANSCRIPTIONS,
+    ENDPOINT__CHAT_COMPLETIONS,
+    ENDPOINT__COMPLETIONS,
+    ENDPOINT__EMBEDDINGS,
+    ENDPOINT__MODELS,
+    ENDPOINT__RERANK,
+)
 
 
 class TeiModelClient(AsyncOpenAI, BaseModelClient):
     ENDPOINT_TABLE = {
-        "audio/transcriptions": None,
-        "chat/completions": None,
-        "embeddings": "v1/embeddings",
-        "models": "info",
-        "rerank": "rerank",
+        ENDPOINT__AUDIO_TRANSCRIPTIONS: None,
+        ENDPOINT__CHAT_COMPLETIONS: None,
+        ENDPOINT__COMPLETIONS: None,
+        ENDPOINT__EMBEDDINGS: "/v1/embeddings",
+        ENDPOINT__MODELS: "/info",
+        ENDPOINT__RERANK: "/rerank",
     }
 
-    def __init__(self, settings: ModelClientSettings, *args, **kwargs) -> None:
+    def __init__(self, model: str, api_url: str, api_key: str, timeout: int) -> None:
         """
         ModelClient class extends OpenAI class to support custom methods.
         """
-        super().__init__(**settings.args.model_dump())
+        self.model = model
+        self.api_url = api_url
+        self.api_key = api_key
+        self.timeout = timeout
+
+        super().__init__(base_url=urljoin(base=self.api_url, url="/v1"), api_key=self.api_key, timeout=self.timeout)
 
         # overwrite OpenAI methods
         self.models.list = partial(_get_models_list, self)
         self.rerank = TEIRerank(client=self)
-
-        self.model = settings.model
 
 
 ########### Overwrite OpenAI methods ############
@@ -40,7 +51,7 @@ def _get_models_list(self, *args, **kwargs) -> Models:
     """
     Custom method to overwrite OpenAI's list method (self.models.list()) and make it synchronous for initialization.
     """
-    url = urljoin(base=str(self.base_url), url=self.ENDPOINT_TABLE["models"])
+    url = urljoin(base=str(self.api_url), url=self.ENDPOINT_TABLE[ENDPOINT__MODELS])
     headers = {"Authorization": f"Bearer {self.api_key}"} if self.api_key else None
 
     response = requests.get(url=url, headers=headers, timeout=self.timeout)
