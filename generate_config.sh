7d959a798167456a8b267d34d83dff9e41240de0
Julien Almarcha <ext-julien.almarcha@flowbird.group>
2025-03-21 09:35:01 +0100

    add more default value in generate config

--- generate_config.sh
+++ generate_config.sh
@@ -3,11 +3,25 @@
 # Default output file is config.yml if no parameter is provided
 OUTPUT_FILE=${1:-config.yml}
 
+# Set default hosts for local development and testing
+POSTGRES_USER=${POSTGRES_USER:-postgres}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-postgres}
+POSTGRES_HOST=${POSTGRES_HOST:-localhost}
+REDIS_HOST=${REDIS_HOST:-localhost}
+REDIS_PASSWORD=${REDIS_PASSWORD:-changeme}
+QDRANT_HOST=${QDRANT_HOST:-localhost}
+QDRANT_API_KEY=${QDRANT_API_KEY:-changeme}
+
 cat config.template.yml | sed \
   -e "s|\${QDRANT_API_KEY}|${QDRANT_API_KEY}|g" \
+  -e "s|\${QDRANT_HOST}|${QDRANT_HOST}|g" \
   -e "s|\${REDIS_PASSWORD}|${REDIS_PASSWORD}|g" \
+  -e "s|\${REDIS_HOST}|${REDIS_HOST}|g" \
   -e "s|\${GRIST_API_KEY}|${GRIST_API_KEY}|g" \
+  -e "s|\${GRIST_DOC_ID}|${GRIST_DOC_ID}|g" \
+  -e "s|\${POSTGRES_USER}|${POSTGRES_USER}|g" \
   -e "s|\${POSTGRES_PASSWORD}|${POSTGRES_PASSWORD}|g" \
+  -e "s|\${POSTGRES_HOST}|${POSTGRES_HOST}|g" \
   -e "s|\${ALBERT_API_KEY}|${ALBERT_API_KEY}|g" \
   > "$OUTPUT_FILE"
 
