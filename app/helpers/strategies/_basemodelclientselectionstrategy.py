d311edf6797310d8cbc0b5192ac975999ab1f0b6
julieandre <julie.andre@illuin.tech>
2025-04-17 17:41:59 +0200

    fix: Map clients with correct type in model router and client classes

--- app/helpers/strategies/_basemodelclientselectionstrategy.py
+++ app/helpers/strategies/_basemodelclientselectionstrategy.py
@@ -2,11 +2,10 @@ from abc import ABC, abstractmethod
 from typing import List
 
 from app.clients.model import BaseModelClient as ModelClient
-from app.schemas.core.settings import Model as ModelSettings
 
 
 class BaseModelClientSelectionStrategy(ABC):
-    def __init__(self, clients: List[ModelSettings]) -> None:
+    def __init__(self, clients: List[ModelClient]) -> None:
         self.clients = clients
 
     @abstractmethod
