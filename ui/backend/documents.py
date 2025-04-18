088d629e1ec9bff6c7daa25b620c57d142bd9c64
leoguillaume <leo.guillaume@modernisation.gouv.fr>
2025-04-18 17:04:54 +0200

    feat: add collection update into playground

--- ui/backend/documents.py
+++ ui/backend/documents.py
@@ -1,4 +1,5 @@
 import time
+from typing import Optional
 
 import requests
 import streamlit as st
@@ -6,9 +7,12 @@ import streamlit as st
 from ui.settings import settings
 
 
-def create_collection(collection_name: str) -> None:
-    headers = {"Authorization": f"Bearer {st.session_state["user"].api_key}"}
-    response = requests.post(f"{settings.playground.api_url}/v1/collections", json={"name": collection_name}, headers=headers)
+def create_collection(collection_name: str, collection_description: str) -> None:
+    response = requests.post(
+        url=f"{settings.playground.api_url}/v1/collections",
+        json={"name": collection_name, "description": collection_description},
+        headers={"Authorization": f"Bearer {st.session_state["user"].api_key}"},
+    )
 
     if response.status_code != 201:
         st.toast(response.json()["detail"], icon="❌")
@@ -19,9 +23,33 @@ def create_collection(collection_name: str) -> None:
     st.rerun()
 
 
+def update_collection(collection_id: int, collection_name: Optional[str] = None, collection_description: Optional[str] = None) -> None:
+    params = {}
+    if collection_name:
+        params["name"] = collection_name
+    if collection_description:
+        params["description"] = collection_description
+
+    response = requests.patch(
+        url=f"{settings.playground.api_url}/v1/collections/{collection_id}",
+        json=params,
+        headers={"Authorization": f"Bearer {st.session_state["user"].api_key}"},
+    )
+
+    if response.status_code != 204:
+        st.toast(response.json()["detail"], icon="❌")
+        return
+
+    st.toast("Update succeed", icon="✅")
+    time.sleep(0.5)
+    st.rerun()
+
+
 def delete_collection(collection_id: int) -> None:
-    headers = {"Authorization": f"Bearer {st.session_state["user"].api_key}"}
-    response = requests.delete(url=f"{settings.playground.api_url}/v1/collections/{collection_id}", headers=headers)
+    response = requests.delete(
+        url=f"{settings.playground.api_url}/v1/collections/{collection_id}",
+        headers={"Authorization": f"Bearer {st.session_state["user"].api_key}"},
+    )
 
     if response.status_code != 204:
         st.toast(response.json()["detail"], icon="❌")
