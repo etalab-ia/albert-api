53f1742a2fbe6f03e0385cc9274ce65355a0fc6e
julieandre <julie.andre@illuin.tech>
2025-04-22 20:46:24 +0200

    test: Add router get client test

---
+++ app/tests/test_router.py
@@ -0,0 +1,15 @@
+from app.utils.lifespan import context
+
+
+class TestModels:
+    def test_get_model_client(self):
+        # Get a language model with more than 1 client
+        router = context.models(model="albert-small")
+
+        # With roundrobin client should be different at each call
+        client_1 = router.get_client(endpoint="")
+        client_2 = router.get_client(endpoint="")
+        client_3 = router.get_client(endpoint="")
+
+        assert client_1.timeout != client_2.timeout
+        assert client_1.timeout == client_3.timeout
