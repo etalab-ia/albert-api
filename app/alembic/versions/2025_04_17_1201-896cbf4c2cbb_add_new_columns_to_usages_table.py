8d1b6151b99c7e209c0c2cf3ae8516938d981ece
Julien Almarcha <ext-julien.almarcha@flowbird.group>
2025-04-23 17:14:33 +0200

    merge migration

--- app/alembic/versions/2025_04_17_1201-896cbf4c2cbb_add_new_columns_to_usages_table.py
+++ app/alembic/versions/2025_04_17_1201-896cbf4c2cbb_add_new_columns_to_usages_table.py
@@ -14,7 +14,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision: str = "896cbf4c2cbb"
-down_revision: Union[str, None] = "8c5ae2a3d4d0"
+down_revision: Union[str, None] = "a4ac45e7c990"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
