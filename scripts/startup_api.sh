5de60bb0d04659e7d3cca0376851cba686eedacc
Julien Almarcha <julien.almarcha@com>
2025-05-06 17:27:26 +0200

    startup scripts dev enhancements

--- scripts/startup_api.sh
+++ scripts/startup_api.sh
@@ -9,6 +9,17 @@ KEEP_ALIVE=${KEEP_ALIVE:-75}
 GRACEFUL_TIMEOUT=${GRACEFUL_TIMEOUT:-75}
 GUNICORN_CMD_ARGS=${GUNICORN_CMD_ARGS:-""} # ex: --log-config app/log.conf
 
+# Set default hosts if not already defined
+if [ -z "$POSTGRES_HOST" ]; then
+  export POSTGRES_HOST=localhost
+fi
+if [ -z "$REDIS_HOST" ]; then
+  export REDIS_HOST=localhost
+fi
+if [ -z "$QDRANT_HOST" ]; then
+  export QDRANT_HOST=localhost
+fi
+
 # Run database migrations
 python -m alembic -c app/alembic.ini upgrade head
 
