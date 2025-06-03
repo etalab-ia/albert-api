ad9088ae2a220326123d9c0ee232949796910113
Benjamin PILIA <benjamin.pilia@protonmail.com>
2025-06-03 17:14:09 +0200

    to reset

--- app/tests/integ/test_mcp.py
+++ app/tests/integ/test_mcp.py
@@ -11,19 +11,32 @@ from app.utils.variables import ENDPOINT__AGENTS_TOOLS, ENDPOINT__AGENTS_COMPLET
 
 @pytest.mark.usefixtures("client")
 class TestMCP:
-    def test_mcp_chat_completions_route_returns_chat_completion_with_tool_call(self, client: TestClient):
-        params = {
-            "model": "albert-large",
-            "messages": [{"role": "user", "content": "Donne moi la météo à Miami"}],
-            "agents": ["get_forecast"],
-            "stream": False,
-            "n": 1,
-            "max_tokens": 200,
-        }
-        response = client.post_without_permissions(url=f"/v1{ENDPOINT__AGENTS_COMPLETIONS}", json=params)
-        assert response.status_code == 200, response.text
-
-        ChatCompletion(**response.json())
+    # def test_mcp_chat_completions_route_returns_chat_completion_with_tool_call(self, client: TestClient):
+    #     params = {
+    #         "model": "albert-large",
+    #         "messages": [{"role": "user", "content": "Donne moi la météo à Miami"}],
+    #         "agents": ["get_forecast"],
+    #         "stream": False,
+    #         "n": 1,
+    #         "max_tokens": 200,
+    #     }
+    #     response = client.post_without_permissions(url=f"/v1{ENDPOINT__AGENTS_COMPLETIONS}", json=params)
+    #     assert response.status_code == 200, response.text
+    #
+    #     ChatCompletion(**response.json())
+    # def test_mcp_chat_completions_route_returns_chat_completion_with_tool_call(self, client: TestClient):
+    #     params = {
+    #         "model": "albert-large",
+    #         "messages": [{"role": "user", "content": "Quelles sont les données sur les accidents de la route ?"}],
+    #         "agents": ["search_datasets", "get_dataset_details"], # mcp_tools
+    #         "stream": False,
+    #         "n": 1,
+    #         "max_tokens": 2000,
+    #     }
+    #     response = client.post_without_permissions(url=f"/v1{ENDPOINT__AGENTS_COMPLETIONS}", json=params)
+    #     assert response.status_code == 200, response.text
+    #
+    #     ChatCompletion(**response.json())
 
     def test_mcp_list_tools_return_tool_list(self, client: TestClient):
         # WHEN
