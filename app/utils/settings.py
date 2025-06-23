4b00888901ba0efc9f9cbb528655c50973243b2d
Benjamin PILIA <benjamin.pilia@protonmail.com>
2025-06-23 16:38:37 +0200

    Refacto lecture .env + fix launch api

--- app/utils/settings.py
+++ app/utils/settings.py
@@ -1,12 +1,6 @@
-import os
 from functools import lru_cache
 
-from dotenv import load_dotenv
-
 from app.schemas.core.settings import Settings
-
-env_file = os.getenv("ENV_FILE", ".env")
-load_dotenv(env_file)
 
 
 @lru_cache
