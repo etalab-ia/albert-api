f1ea7ee6f136881202000a08ea66f3768e17bd19
Matthieu Bonnemains <matthieu.bonnemains@student-cs.fr>
2025-07-18 11:14:08 +0200

    new sql model

--- app/sql/models.py
+++ app/sql/models.py
@@ -128,18 +128,19 @@ class Document(Base):
 class ModelRouter(Base):
     __tablename__ = "modelrouter"
 
-    id = Column(String, primary_key=True, index=True)
+    name = Column(String, primary_key=True, index=True)
     type = Column(String, nullable=False)
     routing_strategy = Column(String, nullable=False)
     from_config = Column(Boolean, nullable=False)
+    owned_by = Column(String, nullable=False)
 
 
 class ModelRouterAlias(Base):
     __tablename__ = "modelrouteralias"
 
-    id = Column(Integer, primary_key=True, index=True)
+    name = Column(Integer, primary_key=True, index=True)
     alias = Column(String, nullable=False)
-    model_router_id = Column(String, ForeignKey(column="modelrouter.id", ondelete="CASCADE"), nullable=False)
+    model_router_name = Column(String, ForeignKey(column="modelrouter.name", ondelete="CASCADE"), nullable=False)
 
     model_router = relationship(argument="ModelRouter", backref=backref(name="alias", cascade="all, delete-orphan"))
 
@@ -148,17 +149,17 @@ class ModelClient(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     url = Column(String, nullable=False)
-    key = Column(String, nullable=False)
+    key = Column(String, nullable=True)
     timeout = Column(Integer, nullable=False)
     model_name = Column(String, nullable=False)
-    model_carbon_footprint_zone = Column(Integer, nullable=False)
-    model_carbon_footprint_total_params = Column(Integer, nullable=False)
-    model_carbon_footprint_active_params = Column(Integer, nullable=False)
+    model_carbon_footprint_zone = Column(String, nullable=False)
+    model_carbon_footprint_total_params = Column(Integer, nullable=True)
+    model_carbon_footprint_active_params = Column(Integer, nullable=True)
     model_cost_prompt_tokens = Column(Float, nullable=False)
     model_cost_completion_tokens = Column(Float, nullable=False)
     metrics_retention_ms = Column(Integer, nullable=False)
     type = Column(String, nullable=False)
 
-    model_router_id = Column(String, ForeignKey(column="modelrouter.id", ondelete="CASCADE"), nullable=False)
+    model_router_name = Column(String, ForeignKey(column="modelrouter.name", ondelete="CASCADE"), nullable=False)
     
     model_router = relationship(argument="ModelRouter", backref=backref(name="client", cascade="all, delete-orphan"))
